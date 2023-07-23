8dd448393e073e53117bd2b0eef992d8e0a7a021
Nikola Ratinac <ratinac.nikola@gmail.com>
2023-07-23 12:34:56 +0200

    update MyDataset to use a path param

--- data.py
+++ data.py
@@ -1,17 +1,14 @@
-from datasets import load_dataset
 import pandas as pd
+from datasets import load_dataset
 
-class MyDataset():
-    def __init__(self):
-    
-        self.dataset = load_dataset("roszcz/internship-midi-data-science", split="train")
+
+class MyDataset:
+    def __init__(self, path: str = "roszcz/internship-midi-data-science"):
+        self.dataset = load_dataset(path, split="train")
         self.record = self.dataset[0]
-        
-    
+
     def __getitem__(self, index):
-        return pd.DataFrame(self.dataset[index]['notes'])
-    
+        return pd.DataFrame(self.dataset[index]["notes"])
+
     def __len__(self):
         return len(self.df)
-
-
